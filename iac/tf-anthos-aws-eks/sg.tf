025f6976f267e92156b2daa83553f24976bbe3ca
Sameer Jain <sameer.jain@isxfinancial.com>
2023-11-02 14:58:48 +1100

    updated sg

--- iac/tf-anthos-aws-eks/sg.tf
+++ iac/tf-anthos-aws-eks/sg.tf
@@ -15,3 +15,19 @@ resource "aws_security_group" "additional" {
 
   tags = merge(local.tags, { Name = "${local.name}-additional" })
 }
+
+resource "aws_security_group" "rds" {
+  name_prefix = "${local.name}-rds"
+  description = "Allow PostgreSQL inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  tags = local.tags
+}
